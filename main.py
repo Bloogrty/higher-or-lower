0d07c34cdb3aea2b0ec751a1c23022ba10444693
Cristanto <cristanto.sidharta@gmail.com>
2025-06-28 12:26:48 +0700

    Update: Add DB, Add Top 10, Update README

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from art import logo, vs
 from game_data import data
 import random
+from database import init_db, insert_score, get_scores
 
 
 def format_data(product):
@@ -17,8 +18,17 @@ def check_answer(user_guess, a_price, b_price):
         return user_guess == "b"
 
 
+def show_high_scores():
+    print("\n=== Top 10 High Scores ===")
+    scores = get_scores()
+    top_scores = scores[:10]
+    for idx, (name, score) in enumerate(top_scores, start=1):
+        print(f"{idx}. {name} - {score} points ")
+
+
 def game():
     print(logo)
+    init_db()
     score = 0
 
     # generate random product
@@ -59,7 +69,14 @@ def game():
             print(
                 f"Wrong guess! Final score: {score}. No more caviar tech dreams for you."
             )
-            return False
+            break
+
+    # save score
+    player_name = input("Enter your name, mighty spender: ").strip() or "Anonymous"
+    insert_score(player_name, score)
+
+    # Show score
+    show_high_scores()
 
 
 game()
